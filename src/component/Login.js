7e4821f13451d634b06f05effb6994df04a659ca
jingtheanteng <jingthean.teng@gmail.com>
2023-12-15 12:37:01 +0700

    Update APIs

--- src/component/Login.js
+++ src/component/Login.js
@@ -1,73 +1,94 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
+
+import $ from 'jquery'
+
+
 import '../css/Login.css'
 
 
-function logins() {
+
+const Login = () => {
     const register = '/register'
 
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleEmailChange = (e) => setEmail(e.target.value);
+    const handlePasswordChange = (e) => setPassword(e.target.value);
+
+    const handleLogin = async (e) => {
+        e.preventDefault();
+
+        $.ajax({
+            url: 'http://127.0.0.1:5000/user/authenticate',
+            type: 'POST',
+            dataType: 'json',
+            contentType: 'application/json',  // Set content type to JSON
+            data: JSON.stringify({
+                email: email,  // Replace with your actual email
+                password: password,    // Replace with your actual password
+            }),
+
+            success: function (response) {
+                // Update state with fetched data
+                if (response) console.log(response)
+            },
+            error: function (error) {
+                console.error('Error:', error);
+            },
+        });
+    };
+
+
     return (
-        <div className="bodyss ">
-            {/* <header>
-                <nav className="navbar navbar-expand-lg navbar-light bg-light border-header">
-                    <div className="container-fluid">
-                        <a className="navbar-brand" href="/Project/Budget-Analysis.html"><b><span>Trip</span><span>Wallet</span></b></a>
-                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                            <span className="navbar-toggler-icon"></span>
-                        </button>
-                        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                            <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-                                <li className="nav-item">
-                                    <a className="nav-link" aria-current="page" href="/Tripwallet/log&register/register.html"><span className= "home">LOGIN/REGISTER</span></a>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                </nav>
-            </header> */}
-            <div className="wrapper wrappers">
-                <div className="form-box login form-boxs border-box">
-                    
-                    <div className="blur">
-                        <div>
-                            <h2 className="tripwallet">Welcome<span> TripWallet</span></h2>
-                        </div>
-                        <form action="#">
-                            <div className="input-box input-boxss"> 
-                                <span className="icon"><ion-icon name="mail-outline"></ion-icon></span>
-                                <input
-                                    type="email"
-                                    // value={email}
-                                    // onChange={handleEmailChange}
-                                    required
-                                />
-                                <label>Email</label>
-                            </div>
-                            <div className="input-box input-boxss"> 
-                                <span className="icon"><ion-icon name="lock-closed-outline"></ion-icon></span>
-                                <input
-                                    type="password"
-                                    // value={password}
-                                    // onChange={handlePasswordChange}
-                                    required
-                                />
-                                <label>Password</label>
-                            </div>
-            
-                            <button type="submit" className="log">Login</button>
-                            <div className=" login-register"> 
-                                <p>Don't have an account? <a href={register} className="register-link">Register</a></p>
-                            </div>
-                        </form>
-                    </div>
+        <div className="bodyss">
+          <div className="wrapper wrappers">
+            <div className="form-box login form-boxs border-box">
+              <div className="blur">
+                <div>
+                  <h2 className="tripwallet">Welcome<span> TripWallet</span></h2>
                 </div>
-            </div>  
-    
-     
+                <form onSubmit={handleLogin}>
+                    <div className="input-box input-boxss"> 
+                        <span className="icon"><ion-icon name="mail-outline"></ion-icon></span>
+                        <input
+                            type="email"
+                            value={email}
+                            onChange={handleEmailChange}
+                            required
+                        />
+                        <label>Email</label>
+                    </div>
+                    <div className="input-box input-boxss"> 
+                        <span className="icon"><ion-icon name="lock-closed-outline"></ion-icon></span>
+                        <input
+                            type="password"
+                            value={password}
+                            onChange={handlePasswordChange}
+                            required
+                        />
+                        <label>Password</label>
+                    </div>
+                  <button type="submit" className="log">
+                    Login
+                  </button>
+                  <div className="login-register">
+                    <p>
+                      Don't have an account?{' '}
+                      <a href={register} className="register-link">
+                        Register
+                      </a>
+                    </p>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
         </div>
-    );
+      );
+    
 }
 
 
-const Login = logins
 
 export default Login
--- src/component/category.js
+++ src/component/category.js
@@ -1,8 +1,48 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import '../css/category.css';
 import landscape1 from '../img/landscape1.png';
 
+import $ from 'jquery'
+
+// const fs = require('fs');
+
+// // Read the configuration file
+// const rawdata = fs.readFileSync('config.json');
+// const config = JSON.parse(rawdata);
+
+// // Access individual properties
+// const http = config.http;
+// const ip = config.ip;
+// const port = config.port;
+
+
 const Category = () => {
+
+    const [categories, setCategories] = useState([]);
+
+    function getCategories() {
+        $.ajax({
+            url: 'http://127.0.0.1:5000/category',
+            type: 'GET',
+            dataType: 'json',
+            success: function (response) {
+                // Update state with fetched data
+                setCategories(response.categories);
+                if (response) console.log(response.categories)
+            },
+            error: function (error) {
+                console.error('Error:', error);
+            },
+        });
+    }
+
+    // Use useEffect to call the function when the component mounts
+    useEffect(() => {
+        getCategories();
+    }, []); // Empty dependency array ensures it only runs once
+
+
+
     return (
         <article>
             <div className="nest-beach-boxshow">
@@ -10,37 +50,15 @@ const Category = () => {
                     <h4 className="main-caption caption-category">Category</h4>
                 </div>
                 <div className="show-category">
-                    <div className="card" style={{ width: '18rem' }}>
-                        <img src={landscape1} className="card-img-top" alt="..." />
-                        <div className="card-body">
-                            <h5 className="card-title">Card title</h5>
-                            <p className="card-text">Detail1</p>
+                    {categories.map((category) => (
+                        <div className="card" style={{ width: '18rem' }} key={category.id}>
+                            <img src={category.image_url || category.image || landscape1} className="card-img-top" alt="..." />
+                            <div className="card-body">
+                                <h5 className="card-title">{category.name}</h5>
+                                <p className="card-text">{category.detail}</p>
+                            </div>
                         </div>
-                    </div>
-                    {/* <div className="card" style={{ width: '18rem' }}>
-                        <img src={landscape1} className="card-img-top" alt="..." />
-                        <div className="card-body">
-                            <h5 className="card-title">Card title</h5>
-                            <p className="card-text">Detail2</p>
-                        </div>
-                        <span className="primary"><a href="#" className="btn ">See more...</a></span>
-                    </div>
-                    <div className="card" style={{ width: '18rem' }}>
-                        <img src={landscape1} className="card-img-top" alt="..." />
-                        <div className="card-body">
-                            <h5 className="card-title">Card title</h5>
-                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        </div>
-                        <span className="primary"><a href="#" className="btn ">See more...</a></span>
-                    </div>
-                    <div className="card" style={{ width: '18rem' }}>
-                        <img src={landscape1} className="card-img-top justify-start" alt="..." />
-                        <div className="card-body">
-                            <h5 className="card-title">Card title</h5>
-                            <p className="card-text"></p>
-                        </div>
-                        <span className="primary"><a href="#" className="btn ">See more...</a></span>
-                    </div> */}
+                    ))}
                 </div>
             </div>
         </article>
